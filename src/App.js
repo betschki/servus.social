8e77f128fe1e20f76416b5262bbf5f6d270a303a
Jannis Betschki <j.betschki@gmail.com>
2020-10-31 17:15:42 +0100

    plausible.io moved to react-helmet

--- src/App.js
+++ src/App.js
@@ -18,6 +18,12 @@ function App() {
           href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
+        <script
+          async
+          defer
+          data-domain="servus.social"
+          src="https://plausible.io/js/plausible.js"
+        ></script>
       </Helmet>
       <TopBar color="yellow" />
       <Container>
